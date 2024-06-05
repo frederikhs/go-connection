0dba2c47b93282aa2d853a8bd31853f5cac03931
Frederik Hørgreen <frederik@hoergreen.dk>
2024-06-05 15:59:11 +0200

    convert tests to use test containers and use testify/assert to clean up error asserts and more

--- Makefile
+++ Makefile
@@ -10,17 +10,11 @@ lint:
 	golangci-lint run
 
 test:
-	go test -race -v ./...
+	go test -v ./...
 
 cover:
-	go test -race -cover -coverprofile=coverage.out -v ./...
+	go test -cover -coverprofile=coverage.out -v ./...
 	go tool cover -html=coverage.out
 
 clean:
 	git clean -fxd -e .idea
-
-start-db:
-	docker run --name test-db -p 3671:5432 -d -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -e POSTGRES_DB=test postgres:alpine
-
-stop-db:
-	docker rm --force test-db
--- connection_test.go
+++ connection_test.go
@@ -1,103 +1,118 @@
 package connection
 
 import (
-	"errors"
-	"os"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"testing"
+	"time"
 )
 
-func givenDatabaseConnectionFromEnv() *Conn {
-	_ = os.Setenv("DB_USER", "test")
-	_ = os.Setenv("DB_PASS", "test")
-	_ = os.Setenv("DB_DATABASE", "test")
-	_ = os.Setenv("DB_HOST", "localhost")
-	_ = os.Setenv("DB_PORT", "3671")
+func givenLaunchedPostgresContainerAndConfig(t *testing.T) *Config {
+	ctx := context.Background()
 
-	return ConnectFromEnv()
+	dbName := "aaa"
+	dbUser := "bbb"
+	dbPassword := "ccc"
+
+	postgresContainer, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("docker.io/postgres:16-alpine"),
+		postgres.WithDatabase(dbName),
+		postgres.WithUsername(dbUser),
+		postgres.WithPassword(dbPassword),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(10*time.Second)),
+	)
+	assert.NoError(t, err)
+
+	t.Cleanup(func() {
+		err = postgresContainer.Terminate(ctx)
+		assert.NoError(t, err)
+	})
+
+	containerPort, err := postgresContainer.MappedPort(ctx, "5432/tcp")
+	assert.NoError(t, err)
+
+	host, err := postgresContainer.Host(ctx)
+	assert.NoError(t, err)
+
+	return NewConfig(dbUser, dbPassword, host, containerPort.Port(), dbName, SSLModeDisable)
 }
 
-func TestConnectCanBeginAndRollbackTransaction(t *testing.T) {
-	connection := givenDatabaseConnectionFromEnv()
+func TestTransactions(t *testing.T) {
+	connection := givenLaunchedPostgresContainerAndConfig(t).Connect()
 
-	if connection.db == nil {
-		t.Fatalf("database property nil")
-	}
+	t.Run("TestConnectCanBeginAndRollbackTransaction", func(t *testing.T) {
+		testConnectCanBeginAndRollbackTransaction(t, connection)
+	})
 
-	if connection.tx != nil {
-		t.Fatalf("transaction property not nil")
-	}
+	t.Run("TestCommit", func(t *testing.T) {
+		testCommit(t, connection)
+	})
+
+	t.Run("TestRollback", func(t *testing.T) {
+		testRollback(t, connection)
+	})
+
+	t.Run("TestBegin", func(t *testing.T) {
+		testBegin(t, connection)
+	})
+}
+
+func testConnectCanBeginAndRollbackTransaction(t *testing.T, connection *Conn) {
+	assert.NotNil(t, connection.db)
+	assert.Nil(t, connection.tx)
 
 	err := connection.Begin()
-	if err != nil {
-		t.Fatalf("failed to begin transaction: %v", err)
-	}
-
-	if connection.tx == nil {
-		t.Fatalf("expected transaction not to be nil when transaction has begun")
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, connection.tx)
 
 	err = connection.Rollback()
-	if err != nil {
-		t.Fatalf("failed to rollback transaction: %v", err)
-	}
+	assert.NoError(t, err)
+	assert.Nil(t, connection.tx)
 }
 
-func TestCommit(t *testing.T) {
-	connection := givenDatabaseConnectionFromEnv()
-
+func testCommit(t *testing.T, connection *Conn) {
 	err := connection.Commit()
-	if err == nil || !errors.Is(err, ErrTransactionNotStarted) {
-		t.Fatalf("expected commit to fail when no transaction has begun")
-	}
-
-	_ = connection.Begin()
-
-	_, err = connection.Exec("CREATE TABLE test (id varchar)")
-	if err != nil {
-		t.Fatalf("failed to execute query: %v", err)
-	}
-
-	_, err = connection.Exec("INSERT INTO test (id) VALUES ($1), ($2), ($3)", "1", "3", "5")
-	if err != nil {
-		t.Fatalf("failed to insert values into testing table: %v", err)
-	}
-
-	err = connection.Commit()
-	if err != nil {
-		t.Fatalf("failed to commit transaction: %v", err)
-	}
-
-	_ = connection.Begin()
-	_, err = connection.Exec("DROP TABLE test")
-	if err != nil {
-		t.Fatalf("failed to drop table: %v", err)
-	}
-
-	err = connection.Commit()
-	if err != nil {
-		t.Fatalf("failed to commit transaction: %v", err)
-	}
-}
-
-func TestRollback(t *testing.T) {
-	connection := givenDatabaseConnectionFromEnv()
-
-	err := connection.Rollback()
-	if err == nil || !errors.Is(err, ErrTransactionNotStarted) {
-		t.Fatalf("expected rollback to fail when no transaction has begun")
-	}
-}
-
-func TestBegin(t *testing.T) {
-	connection := givenDatabaseConnectionFromEnv()
-
-	err := connection.Begin()
-	if err != nil {
-		t.Fatalf("failed to begin transaction: %v", err)
-	}
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, ErrTransactionNotStarted)
 
 	err = connection.Begin()
-	if err == nil || !errors.Is(err, ErrTransactionAlreadyStarted) {
-		t.Fatalf("expected begin to fail when transaction has already begun")
-	}
+	assert.NoError(t, err)
+
+	_, err = connection.Exec("CREATE TABLE test (id varchar)")
+	assert.NoError(t, err)
+
+	_, err = connection.Exec("INSERT INTO test (id) VALUES ($1), ($2), ($3)", "1", "3", "5")
+	assert.NoError(t, err)
+
+	err = connection.Commit()
+	assert.NoError(t, err)
+
+	err = connection.Begin()
+	assert.NoError(t, err)
+	_, err = connection.Exec("DROP TABLE test")
+	assert.NoError(t, err)
+
+	err = connection.Commit()
+	assert.NoError(t, err)
+}
+
+func testRollback(t *testing.T, connection *Conn) {
+	err := connection.Rollback()
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, ErrTransactionNotStarted)
+}
+
+func testBegin(t *testing.T, connection *Conn) {
+	err := connection.Begin()
+	assert.NoError(t, err)
+
+	err = connection.Begin()
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, ErrTransactionAlreadyStarted)
 }
